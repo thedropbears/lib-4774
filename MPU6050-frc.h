946f43ea9c5aa9442f67251f1b32aaa943562652
Lucien <lucienp@live.com.au>
2014-05-19 20:52:37 +1000

    changd the extentions to public so that they can be used in code

--- MPU6050-frc.h
+++ MPU6050-frc.h
@@ -4,7 +4,7 @@
 #include "WPILib.h"
 #include "MPU6050.h"
 
-class MPU6050_frc : public SensorBase, PIDSource, MPU6050 {
+class MPU6050_frc : public SensorBase, public PIDSource, public MPU6050 {
     public:
 	    MPU6050_frc();
         MPU6050_frc(uint8_t address);
