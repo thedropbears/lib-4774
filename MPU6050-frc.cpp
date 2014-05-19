6b5f92c35baff6ce9d327fdfe7c246c90dde1e36
Lucien <lucienp@live.com.au>
2014-05-19 20:06:09 +1000

    Implemented the Zaxis tracking code

--- MPU6050-frc.cpp
+++ MPU6050-frc.cpp
@@ -1,3 +1,16 @@
 #include "MPU6050-frc.h"
 
+MPU6050_frc :: MPU6050_frc(){
+	initialize();
+    //dmpInitialize();
+    setXGyroOffset(220);
+    setYGyroOffset(76);
+    setZGyroOffset(-85);
+    setZAccelOffset(1788);
+    //setDMPEnabled(true);
+    
+}
 
+double MPU6050_frc :: PIDGet() {
+    return getRotationZ();
+}
--- MPU6050-frc.h
+++ MPU6050-frc.h
@@ -1,36 +1,14 @@
-#ifndef MPU6050-frc_h
-#define MPU6050-frc_h
+#ifndef MPU6050_frc_h
+#define MPU6050_frc_h
 
 #include "WPILib.h"
+#include "MPU6050.h"
 
-
-class MPU6050 : public SensorBase, PIDSource {
+class MPU6050_frc : public SensorBase, PIDSource, MPU6050 {
     public:
+	    MPU6050_frc();
         virtual double PIDGet();
-        
-        enum Axes {kAxis_X=0x00, kAxis_Y=0x02, kAxis_Z=0x04};
-        struct AllAxes{
-            double XAxis;
-            double YAxis;
-            double ZAxis;
-        };
-        
-        virtual double GetAngle(Axes axis);
-        virtual double GetRate(Axes axis);
-        virtual void Reset();
-        
-        virtual double GetAcceleration(Axes axis);
-        virtual AllAxes GetAccelerations();
-    private:
-        virtual void InitGyro();
-        virtual void InitImu();
-    protected:
-        I2C* m_i2c;
-        static const uint8_t kAddress = 0x69;
-        static const uint8_t kPowerCtlRegister = 0x2D;
-        static const uint8_t kDataFormatRegister = 0x31;
-        static const uint8_t kDataRegister = 0x32;
-        static constexpr double kGsPerLSB = 0.00390625;
+
 };
 
 #endif
