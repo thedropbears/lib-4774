893fcd8331a9e0deccf30ea5a190a14d7ab363bb
James Ward <j.ward@acfr.usyd.edu.au>
2014-05-26 17:38:15 +1000

    Fixed constructors inMPU6050.

--- MPU6050.cpp
+++ MPU6050.cpp
@@ -1,24 +1,22 @@
 #define __arm__
 #define __PGMSPACE_H_
 #define delay(p) Wait(p)
-#include "mpu6050/MPU6050_6Axis_MotionApps20.h"
+//#include "mpu6050/MPU6050_6Axis_MotionApps20.h"
 #include "MPU6050.h"
 
-lib4774::MPU6050::MPU6050(){
+lib4774::MPU6050::MPU6050() : ::MPU6050(MPU6050_DEFAULT_ADDRESS << 1){
 	// We have to left shift the address by one because the WPI I2C
 	// library doesn't do it for us.
 	// It includes the R/W bit - so we need to account for it.
 	// I2C addresses are 7 bits, but WPILib assumes we have the full 8.
-	::MPU6050(MPU6050_DEFAULT_ADDRESS << 1);
     run();
 }
 
-lib4774::MPU6050::MPU6050(uint8_t address){
+lib4774::MPU6050::MPU6050(uint8_t address): ::MPU6050(address << 1){
 	// We have to left shift the address by one because the WPI I2C
 	// library doesn't do it for us.
 	// It includes the R/W bit - so we need to account for it.
 	// I2C addresses are 7 bits, but WPILib assumes we have the full 8.
-    ::MPU6050(address << 1);
     run();
 }
 
