9ed8bc3feb5c9b27672d1d9760ec8f9dd5dba4b7
ArthurAllshire <arthur.allshire@gmail.com>
2014-11-09 06:56:27 +1100

    Incrememt strtod pointer

--- subsystems/DropBoneImu.cpp
+++ subsystems/DropBoneImu.cpp
@@ -12,7 +12,7 @@ int DropBoneImu::parsePacket(char* recv_buffer, int received_bytes) {
     char* end = recv_buffer;
     double next;
     for(int i = 0; i < PARSEDLEN; i++){
-        next=strtod(end,&end);
+        next=strtod(end+1,&end);
         parsed[i] = next;
     }
     return 0;
