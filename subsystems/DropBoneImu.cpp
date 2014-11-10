33f9407c0623a9b725d456b6743b89519e2c6509
James Ward <j.ward@acfr.usyd.edu.au>
2014-11-10 17:09:24 +1100

    Minor bugfixes.

--- subsystems/DropBoneImu.cpp
+++ subsystems/DropBoneImu.cpp
@@ -13,8 +13,11 @@ DropBoneImu::~DropBoneImu() {
 int DropBoneImu::parsePacket(char* recv_buffer, int received_bytes) {
     char* end = recv_buffer;
     double next;
-    for(int i = 0; i < BONE_PARSEDLEN; i++){
-        next=strtod(end+1,&end);
+    for(int i = 0; i < BONE_PARSEDLEN-1; i++){
+        next=strtod(end,&end);
+        if (end != NULL) {
+			end++; // skip the comma
+		}
         parsed[i] = next;
     }
     return 0;
@@ -37,13 +40,13 @@ double* DropBoneImu::getQuat() {
 }
 
 double DropBoneImu::getYawAngle() {
-    return parsed[0];
-}
-
-double DropBoneImu::getYawRate() {
     double yaw_angle;
     yaw_angle = atan2(sin(parsed[0]-offset),cos(parsed[0]-offset));
     return yaw_angle;
+}
+
+double DropBoneImu::getYawRate() {
+    return parsed[5];
 }
 
 double DropBoneImu::PIDGet() {
