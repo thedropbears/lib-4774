f89e902aad8b0b620bb0ff922e3cbd3ea3a18ed6
ArthurAllshire <arthur.allshire@gmail.com>
2015-01-07 08:19:55 +0000

    recommit dropbone

---
+++ libs/drop_bone_imu.c
@@ -0,0 +1,178 @@
+#include "drop_bone_imu.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <linux/i2c-dev.h>
+#include <unistd.h>
+#include <string.h>
+#include <fcntl.h>
+#include <math.h>
+#include <poll.h>
+#include <sys/ioctl.h>
+#include <time.h>
+
+float last_euler[3] = { 99.9, 99.9, 99.9 };
+float quat_offset[4] = { 0.0F };
+int fd; // file descriptor for the I2C bus
+signed char gyro_orientation[9] = {0,  1,  0,
+        -1, 0,  0,
+        0,  0,  1};
+
+int init(){
+    open_bus();
+    unsigned char whoami=0;
+    i2c_read(MPU6050_ADDR, MPU6050_WHO_AM_I, 1, &whoami);
+    printf("WHO_AM_I: %x\n", whoami);
+    struct int_param_s int_param;
+    printf("MPU init: %i\n", mpu_init(&int_param));
+    printf("MPU sensor init: %i\n", mpu_set_sensors(INV_XYZ_GYRO | INV_XYZ_ACCEL));
+    printf("MPU configure fifo: %i\n", mpu_configure_fifo(INV_XYZ_GYRO | INV_XYZ_ACCEL));
+    printf("DMP firmware: %i\n ",dmp_load_motion_driver_firmware());
+    printf("DMP orientation: %i\n ",dmp_set_orientation(
+        inv_orientation_matrix_to_scalar(gyro_orientation)));
+    unsigned short dmp_features = DMP_FEATURE_6X_LP_QUAT | DMP_FEATURE_TAP | DMP_FEATURE_SEND_RAW_ACCEL | DMP_FEATURE_SEND_CAL_GYRO | DMP_FEATURE_GYRO_CAL;
+    printf("DMP feature enable: %i\n", dmp_enable_feature(dmp_features));
+    printf("DMP set fifo rate: %i\n", dmp_set_fifo_rate(DEFAULT_MPU_HZ));
+    printf("DMP enable %i\n", mpu_set_dmp_state(1));
+    mpu_set_int_level(1); // Interrupt is low when firing
+    dmp_set_interrupt_mode(DMP_INT_CONTINUOUS); // Fire interrupt on new FIFO value
+    return 0;
+}
+
+int i2c_write(unsigned char slave_addr, unsigned char reg_addr,
+        unsigned char length, unsigned char const *data){
+    unsigned char tmp[length+1];
+    tmp[0] = reg_addr;
+    memcpy(tmp+1, data, length);
+    if (write(fd, tmp, length+1) != length + 1){
+        return -1;
+    }
+    return 0; 
+}
+int i2c_read(unsigned char slave_addr, unsigned char reg_addr,
+        unsigned char length, unsigned char *data){
+    if (write(fd,&reg_addr, 1) != 1){
+        return -1;
+    }
+    if  (read(fd,data, length) != length){
+        return -2;
+    }
+
+    return 0;
+}
+
+int open_bus() { 
+    if ((fd = open(BBB_I2C_FILE, O_RDWR)) < 0) {
+        /* ERROR HANDLING: you can check errno to see what went wrong */
+        perror("Failed to open the i2c bus");
+        return 1;
+    }
+    if (ioctl(fd, I2C_SLAVE, MPU6050_ADDR) < 0) {
+        perror("Failed to acquire bus access and/or talk to slave.\n");
+        /* ERROR HANDLING; you can check errno to see what went wrong */
+        return 1;
+    }
+    return 0;
+}
+
+// multiply two quaternions
+int q_multiply(float* q1, float* q2, float* result) {
+    float tmp[4];
+    tmp[0] = q1[0]*q2[0] - q1[1]*q2[1] - q1[2]*q2[2] - q1[3]*q2[3];
+    tmp[1] = q1[0]*q2[1] + q1[1]*q2[0] + q1[2]*q2[3] - q1[3]*q2[2];
+    tmp[2] = q1[0]*q2[2] - q1[1]*q2[3] + q1[2]*q2[0] + q1[3]*q2[1];
+    tmp[3] = q1[0]*q2[3] + q1[1]*q2[2] - q1[2]*q2[1] + q1[3]*q2[0];
+    memcpy(result, tmp, 4*sizeof(float));
+    return 0;
+}
+
+// rescale an array of longs by scale factor into an array of floats
+int rescale_l(long* input, float* output, float scale_factor, char length) {
+    int i;
+    for(i=0;i<length;++i)
+        output[i] = input[i] * scale_factor;
+    return 0;
+}
+
+// rescale an array of shorts by scale factor into an array of floats
+int rescale_s(short* input, float* output, float scale_factor, char length) {
+    int i;
+    for(i=0;i<length;++i)
+        output[i] = input[i] * scale_factor;
+    return 0;
+}
+
+void delay_ms(unsigned long num_ms){
+
+}
+void get_ms(unsigned long *count){
+
+}
+void reg_int_cb(struct int_param_s *param){
+
+}
+
+inline int min ( int a, int b ){
+    return a < b ? a : b;
+}
+inline void __no_operation(){
+
+}
+
+void euler(float* q, float* euler_angles) {
+    euler_angles[0] = -atan2(2*q[1]*q[2] - 2*q[0]*q[3], 2*q[0]*q[0] + 2*q[1]*q[1] - 1); // psi, yaw
+    euler_angles[1] = asin(2*q[1]*q[3] + 2*q[0]*q[2]); // phi, pitch
+    euler_angles[2] = -atan2(2*q[2]*q[3] - 2*q[0]*q[1], 2*q[0]*q[0] + 2*q[3]*q[3] - 1); // theta, roll
+}
+
+// Functions for setting gyro/accel orientation
+unsigned short inv_row_2_scale(const signed char *row)
+{
+    unsigned short b;
+
+    if (row[0] > 0)
+        b = 0;
+    else if (row[0] < 0)
+        b = 4;
+    else if (row[1] > 0)
+        b = 1;
+    else if (row[1] < 0)
+        b = 5;
+    else if (row[2] > 0)
+        b = 2;
+    else if (row[2] < 0)
+        b = 6;
+    else
+        b = 7;      // error
+    return b;
+}
+
+unsigned short inv_orientation_matrix_to_scalar(
+        const signed char *mtx)
+{
+    unsigned short scalar;
+
+    /*
+       XYZ  010_001_000 Identity Matrix
+       XZY  001_010_000
+       YXZ  010_000_001
+       YZX  000_010_001
+       ZXY  001_000_010
+       ZYX  000_001_010
+     */
+
+    scalar = inv_row_2_scale(mtx);
+    scalar |= inv_row_2_scale(mtx + 3) << 3;
+    scalar |= inv_row_2_scale(mtx + 6) << 6;
+
+
+    return scalar;
+}
+
+void advance_spinner() {
+    static char bars[] = { '/', '-', '\\', '|' };
+    static int nbars = sizeof(bars) / sizeof(char);
+    static int pos = 0;
+    printf("%c\b", bars[pos]);
+    fflush(stdout);
+    pos = (pos + 1) % nbars;
+}
---
+++ libs/drop_bone_imu.h
@@ -0,0 +1,79 @@
+
+#ifndef drop_bone_imu_h
+#define drop_bone_imu_h
+
+#define MPU6050 // The Invensense Motion Driver code needs this
+
+#define BBB_I2C_FILE "/dev/i2c-1"
+
+#define MPU6050_ADDR 0x68
+#define MPU6050_WHO_AM_I 0x75
+
+#define DEFAULT_MPU_HZ 10
+
+#define PI 3.14159
+#define QUAT_SCALE (1.0/1073741824)
+#define GPIO_INT_FILE "/sys/class/gpio/gpio14/value" 
+
+#define GYRO_SCALE (PI/(180.0*16.384))
+
+#define ACCEL_SCALE (1.0/16384)
+
+#define NOSENTVALS 13 // the numver of values to be sent through UDP
+
+#define THRESHOLD (0.1*PI/180.0) // the amount that the IMU Euler values have to change less than to indicate that calibration has finished
+
+#define CALIBRATION_TIME 20.0
+
+#include "inv_mpu_dmp_motion_driver.h"
+#include "inv_mpu.h"
+
+
+
+/* The following functions must be defined for this platform:
+ * i2c_write(unsigned char slave_addr, unsigned char reg_addr,
+ *      unsigned char length, unsigned char const *data)
+ * i2c_read(unsigned char slave_addr, unsigned char reg_addr,
+ *      unsigned char length, unsigned char *data)
+ * delay_ms(unsigned long num_ms)
+ * get_ms(unsigned long *count)
+ * reg_int_cb(void (*cb)(void), unsigned char port, unsigned char pin)
+ * labs(long x)
+ * fabsf(float x)
+ * min(int a, int b)
+ */
+
+#define log_i printf
+
+int init();
+
+int i2c_write(unsigned char slave_addr, unsigned char reg_addr,
+        unsigned char length, unsigned char const *data);
+int i2c_read(unsigned char slave_addr, unsigned char reg_addr,
+        unsigned char length, unsigned char *data);
+
+int open_bus();
+
+int q_multiply(float* q1, float* q2, float* result); // multiply two quaternions together {w, x, y, z}
+
+int rescale_l(long* input, float* output, float scale_factor, char length);
+int rescale_s(short* input, float* output, float scale_factor, char length);
+void delay_ms(unsigned long num_ms);
+void get_ms(unsigned long *count);
+void reg_int_cb(struct int_param_s *);
+
+void parse_args(int argc, char** argv);
+void print_usage();
+
+inline int min ( int a, int b );
+inline void __no_operation();
+
+void euler(float* q, float* euler_angles); // Convert quaternions to Euler angles
+
+// Functions for setting gyro/accel orientation
+unsigned short inv_row_2_scale(const signed char *row);
+unsigned short inv_orientation_matrix_to_scalar(const signed char *mtx);
+
+void advance_spinner();
+
+#endif
