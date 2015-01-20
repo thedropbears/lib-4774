536b58f1b99d770045ca7af176c058c7de355c0d
Michael Cirillo <akabunnaman@hotmail.com>
2015-01-20 16:22:31 +1100

    Bug fixes to exponential scaling

--- Functions.cpp
+++ Functions.cpp
@@ -1,13 +1,15 @@
-#include Functions.h
+#include "Functions.h"
+#include "math.h"
 
-namespace lib-4774{
+namespace lib4774{
 	float scaleJoystick (float joystick_value, float exponential, float deadzone)
 	{
 		double a = log(exponential+1)/(1-deadzone);
-		if (joystick_value > 0) {
-			float joystick_scaled = (exp(a*(joystick_value - deadzone))-1)/exponential;
-		} else {
-			float joystick_scaled = -(exp(a*-(joystick_value + deadzone))-1)/exponential;
+		float joystick_scaled = 0;
+		if (joystick_value > deadzone) {
+			joystick_scaled = (exp(a*(joystick_value - deadzone))-1)/exponential;
+		} else if (joystick_value < -deadzone){
+			joystick_scaled = -(exp(a*-(joystick_value + deadzone))-1)/exponential;
 		}
 		return joystick_scaled;
 	}
--- Functions.h
+++ Functions.h
@@ -1,7 +1,7 @@
 #ifndef FUNCTIONS_H
 #define FUNCTIONS_H
 
-namespace lib-4774 {
+namespace lib4774 {
 	float scaleJoystick(float joystick_value, float exponential, float deadzone = 0);
 }
 #endif
